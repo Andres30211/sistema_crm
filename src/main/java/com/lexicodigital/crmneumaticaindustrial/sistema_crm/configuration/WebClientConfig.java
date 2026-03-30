33f63dc5eb08dd831cad887925d7282d20ba56da
Andres30211 <felifit27@gmail.com>
2026-03-30 10:56:24 -0500

    Push para probar la base de datos

--- src/main/java/com/lexicodigital/crmneumaticaindustrial/sistema_crm/configuration/WebClientConfig.java
+++ src/main/java/com/lexicodigital/crmneumaticaindustrial/sistema_crm/configuration/WebClientConfig.java
@@ -2,11 +2,13 @@ package com.lexicodigital.crmneumaticaindustrial.sistema_crm.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
+@EnableWebFluxSecurity
 public class WebClientConfig {
 
 	@Bean
