4a5cbba7b73c78a3349d1ea0861695f13e9c5f40
Andres30211 <felifit27@gmail.com>
2026-03-30 11:11:44 -0500

    Push para probar la base de datos

--- src/main/java/com/lexicodigital/crmneumaticaindustrial/sistema_crm/controller/PostFacebookController.java
+++ src/main/java/com/lexicodigital/crmneumaticaindustrial/sistema_crm/controller/PostFacebookController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,7 +33,7 @@ public class PostFacebookController {
  // --- SECCIÓN WEBHOOK ---
 
     // 1. Validación para Meta (Configúralo en el panel de Developers)
-    @GetMapping("/webhook")
+    @PostMapping("/webhook")
     public ResponseEntity<String> validarWebhook(
             @RequestParam("hub.mode") String mode,
             @RequestParam("hub.verify_token") String token,
