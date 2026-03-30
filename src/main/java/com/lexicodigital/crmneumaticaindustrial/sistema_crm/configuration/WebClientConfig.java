6768bb22ca1ffd34a41a687593e02b1be52bf1a1
Andres30211 <felifit27@gmail.com>
2026-03-30 10:50:14 -0500

    Push para probar la base de datos

--- src/main/java/com/lexicodigital/crmneumaticaindustrial/sistema_crm/configuration/WebClientConfig.java
+++ src/main/java/com/lexicodigital/crmneumaticaindustrial/sistema_crm/configuration/WebClientConfig.java
@@ -2,6 +2,8 @@ package com.lexicodigital.crmneumaticaindustrial.sistema_crm.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -10,5 +12,16 @@ public class WebClientConfig {
 	@Bean
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
+    }
+	
+	@Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+            .csrf(csrf -> csrf.disable()) // Desactiva CSRF para que el POST de Facebook entre
+            .authorizeExchange(exchanges -> exchanges
+                .pathMatchers("/api/v1/facebook/webhook").permitAll() // Permite el webhook sin login
+                .anyExchange().authenticated() // El resto requiere seguridad si la tienes configurada
+            )
+            .build();
     }
 }
