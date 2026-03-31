082d9b58aa5ea3f8821e235e34fe6b3f8df48359
Andres30211 <felifit27@gmail.com>
2026-03-31 18:44:18 -0500

    Push para probar la base de datos

--- src/main/java/com/lexicodigital/crmneumaticaindustrial/sistema_crm/configuration/ConfigDataFacebook.java
+++ src/main/java/com/lexicodigital/crmneumaticaindustrial/sistema_crm/configuration/ConfigDataFacebook.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.PropertySources;
 
 @Configuration
 @PropertySources({
-	@PropertySource("classpath:datafacebook.properties")
+	@PropertySource("classpath:dataFacebook.properties")
 })
 public class ConfigDataFacebook {
 
