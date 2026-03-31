fb84e9df8847519fdd275cb24d9692b23b171080
Andres30211 <felifit27@gmail.com>
2026-03-30 19:32:12 -0500

    Push para probar la base de datos

--- src/main/java/com/lexicodigital/crmneumaticaindustrial/sistema_crm/dto/PostFacebookDto.java
+++ src/main/java/com/lexicodigital/crmneumaticaindustrial/sistema_crm/dto/PostFacebookDto.java
@@ -4,12 +4,14 @@ import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.Id;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
+@Entity
 @Table(name = "post_facebook")
 public class PostFacebookDto {
 	
